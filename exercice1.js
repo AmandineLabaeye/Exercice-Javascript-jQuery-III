58104b26f460c096a6c6425ef3499765b86cc7fc
Amandine <amandine.labaeye@orange.fr>
2018-12-03 10:43:14 +0100

    commit

---
+++ exercice1.js
@@ -0,0 +1,11 @@
+// Stocke le titre grâce à son id pour s'en reservir
+var math = $("#math-heading");
+
+// Ajoute du text à la fin
+math.append(" wow!");
+
+// Stocke le titre grâce à son id pour s'en reservir
+var science = $("#science-heading");
+
+// Ajoute du text à la fin
+science.append(" amazing!");
